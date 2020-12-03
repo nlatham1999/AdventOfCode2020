9c68cc8a2bc73ae072a6a2792d22e2822d2ff324
nick Latham <nlatham@zagmail.gonzaga.edu>
2020-12-02 19:36:34 -0800

    feat: finished day 1

--- src/com/company/DayOne.java
+++ src/com/company/DayOne.java
@@ -1,9 +1,44 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//completes the task for day one
 public class DayOne {
 
     public static int getAnswer() {
+        ArrayList<Integer> numbers = getFile();
+
+        for(int i = 0; i < numbers.size(); i++){
+            for(int j = i + 1; j < numbers.size(); j++){
+                if(numbers.get(i) + numbers.get(j) == 2020){
+                    return numbers.get(i) * numbers.get(j);
+                }
+            }
+            System.out.println("Still Searching...");
+        }
+
         return 0;
+    }
+
+    public static ArrayList<Integer> getFile(){
+        ArrayList<Integer> list = new ArrayList<>();
+        try {
+            File myObj = new File("./src/com/company/DayOneInput");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                list.add(Integer.parseInt(data));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return list;
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("TEST " + DayOne.getAnswer());
+        System.out.println("The Answer Is: " + DayOne.getAnswer());
 	    System.out.println("hello world");
     }
 }
